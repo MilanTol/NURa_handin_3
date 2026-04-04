2907b87d3dced68f2fba7ac92ff9f2b29d0b89bc
Milan Tol <milantol35@gmail.com>
2026-04-04 20:40:51 +0200

    .

--- Q1_SatelliteGalaxies.py
+++ Q1_SatelliteGalaxies.py
@@ -7,7 +7,8 @@ from minimizer import Minimizer
 from levenberg_marquadt import levenberg_marquardt
 from romberg_integrator import romberg_integrator
 from downhill_simplex import downhill_simplex
-
+from gradient import gradient
+from matrix import Matrix
 
 def readfile(filename):
     """
@@ -127,6 +128,90 @@ def get_normalization_constant(a: float, b: float, c: float, Nsat: float, order:
     I = romberg_integrator(func, (0, 5), order=order)
     return Nsat/I
  
+def levenberg_marquardt(
+    model:callable, y_data:np.ndarray, x_data:np.ndarray, err_data:np.ndarray, 
+    p_init:np.ndarray, lmbda_init:float=1e-3, w=10, maxit:int=1000, rel_tol:float = 1e-4
+)->np.ndarray:  
+    """
+    levenberg-marquardt algorithm to find parameters that minimize 
+    chi^2 for the given data and model, adjusted to find a, b, c.
+    This algorithm transitions from steepest descent to newtons method.
+
+    Args:
+        model (callable): 
+            model of which to find optimal parameters
+        y_data (np.ndarray): 
+            y_values of data.
+        x_data (np.ndarray): 
+            x_values of data.
+        err_data (np.ndarray): 
+            errors on y_values of data.
+        p_init (np.ndarray): 
+            initial estimate of optimal parameters.
+        lmbda_init (float, optional): 
+            initial lambda_value, sets the step value for updates. Defaults to 1e-3.
+        w (int, optional): 
+            sets how quickly lmbda updates. Defaults to 10.
+        maxit (int, optional): 
+            maximum iterations the algorithm may take. Defaults to 1000.
+        reltol (float, optional):
+            relative tolerance (if chi_new - chi < reltol*chi algorithm terminates).
+            Defaults to 1e-8.
+
+    Returns:
+        p (np.ndarray): 
+            array containing optimal parameters.
+        min_Chi2 (int):
+            minimum value of Chi2 found.
+    """
+    
+    lmbda = lmbda_init
+    p = p_init.copy()
+    chi = chi2(model, y_data, x_data, err_data, p)
+    # precompute 1/w since we will be using this multiple times:
+    w_inv = 1/w
+
+    for j in range(maxit):
+        # create chi function that only depends on model parameters:
+        chi_temp = lambda args: chi2(model, y_data, x_data, err_data, args)
+        # compute the gradient of chi_temp at p and compute beta
+        beta = -0.5*gradient(chi_temp, p)
+
+        # initialize alpha matrix of size (params, params,):
+        alpha = np.zeros( (len(p), len(p),), dtype=float)
+        # loop over data points:
+        for i in range(len(x_data)):
+            # create model function that only depends on model parameters:
+            model_temp = lambda args: model(x_data[i], *args)
+            # compute the gradient of model_temp at p
+            model_gradient = gradient(model_temp, p)
+            alpha += np.outer(model_gradient, model_gradient) / (err_data[i]*err_data[i])
+        
+        # add lmbda*(diagonal of alpha), this is what makes it the levenberg algorithm
+        alpha += lmbda * np.diag(np.diag(alpha)) # np.diag(np.diag()) extracts the diagonal as a matrix
+
+        # make alpha a matrix and solve alpha@delp = beta:
+        alpha = Matrix(alpha)
+        delp = alpha.solve(beta)
+
+        # compute the chi2 value at the proposed point
+        chi_new = chi2(model, y_data, x_data, err_data, p+delp)
+        # see whether to approve next step
+        Delta_chi = chi_new - chi
+        if Delta_chi < 0: # if new point is better:
+            # check termination condition:
+            if np.abs(Delta_chi) < rel_tol*chi: # chi - chi_new > 0
+                return p + delp, chi_new
+            # otherwise update step
+            p += delp 
+            chi = chi_new
+            lmbda *= w_inv # become more like newton's method
+        else: # if new point is worse: become more like steepest descent
+            lmbda *= w
+
+    print("WARNING (levenberg-marquardt): desired tolerance not reached")
+    return p, chi_new
+
 
 def g(a:float, b:float, c:float, data:np.ndarray, normalization_order:int=5) -> float:
     """
@@ -216,9 +301,6 @@ def do_question_1b():
     for datafile in datafiles:
         radius, nhalo = readfile(f"Data/satgals_{datafile}.txt")
 
-        print(datafile)
-        # TODO: implement the fitting of N(x) to the data using chi-squared minimization.
-
         # we compute the average number of satellites for halos in a mass bin
         # by computing the length of the radius array (which is equal to the number of
         # satellites) and then dividing by the total number of halos (nhalo).
@@ -227,8 +309,7 @@ def do_question_1b():
         N_sat.append(N_sat_temp) 
         
         # we now bin the radii found in the data
-        
-        nbins = 20  # choose appropriate bins
+        nbins = 20  
         # we choose our bin ranges slightly larger
         # than how far our data extends, because empty bins
         # contain information.
