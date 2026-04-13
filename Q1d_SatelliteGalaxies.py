561f6f21eec105843b221a24e39843f527370725
Milan Tol <milantol35@gmail.com>
2026-04-13 10:19:10 +0200

    .

--- Q1d_SatelliteGalaxies.py
+++ Q1d_SatelliteGalaxies.py
@@ -2,12 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from chi2 import chi2
-from minimizer import Minimizer
 from romberg_integrator import romberg_integrator
-from downhill_simplex import downhill_simplex
-from gradient import gradient
-from matrix import Matrix
+from scipy.special import gammainc
 
 
 def readfile(filename):
@@ -132,6 +128,34 @@ def get_normalization_constant(a: float, b: float, c: float, order: int = 5) -> 
     return 1 / I
 
 
+# def get_normalization_constant(a: float, b: float, c: float, order: int = 5) -> float:
+#     """
+#     Calculate the normalization constant A (which is a function of a,b,c) for the satellite number density profile.
+
+#     Parameters
+#     ----------
+#     a : float
+#         Small-scale slope.
+#     b : float
+#         Transition scale.
+#     c : float
+#         Steepness of exponential drop-off.
+#     Nsat : (float, optional)
+#         Average number of satellites. Defaults to 1.
+#     order: int
+#         Order to use for romberg integrator.
+
+#     Returns
+#     -------
+#     float
+#         Normalization constant A.
+#     """
+#     # integrate in logspace
+#     func = lambda logx: np.exp(logx)*satellite_number(x=np.exp(logx), A=1, Nsat=1, a=a, b=b, c=c)
+#     I = romberg_integrator(func, (-15, np.log(5)), order=order)
+#     return 1 / I
+
+
 def Gtest(y_data: np.ndarray, y_model: np.ndarray) -> float:
     """
     returns Gtest estimate of data and model
@@ -144,6 +168,25 @@ def Gtest(y_data: np.ndarray, y_model: np.ndarray) -> float:
         float: Gtest
     """
     return 2 * np.sum(y_data * np.log(y_data / y_model))
+
+
+def Qscore(k:int, x:float) -> float:
+    """
+    computes probability of finding a chi^2 at least
+    as bad as x by chance, given k degrees of freedom.
+
+    Args:
+        k (int): degrees of freedom
+        x (float): minimum chi2 value
+
+    Returns:
+        float: Qscore
+    """
+    # we are allowed to use special library functions 
+    # to compute the gamma functions. 
+    # scipy.special.gammainc computes P(x,k) directly. 
+    Pkx = gammainc(0.5*k, 0.5*x)
+    return 1 - Pkx
 
 
 def do_question_1d():
@@ -173,7 +216,7 @@ def do_question_1d():
     for datafile in datafiles:
         radius, nhalo = readfile(f"Data/satgals_{datafile}.txt")
 
-        # Use best-fit parameters from previous steps
+        # load best fit parameters for current datafile
         p_chi2 = best_params_chi2[datafiles.index(datafile)]
         p_poisson = best_params_poisson[datafiles.index(datafile)]
 
@@ -190,48 +233,57 @@ def do_question_1d():
         bin_counts = np.histogram(radius, bin_edges)[
             0
         ]  # "mean number of satellites in each radial bin"
-        data = bin_counts / (nhalo * bin_widths)
-        Nsat = np.sum(bin_counts) / nhalo
+        data = bin_counts
+        Nsat = np.sum(bin_counts)
 
+        # define the model 
+        def model(bin, a, b, c):
+            func = lambda x: satellite_number(
+                x=x,
+                A=get_normalization_constant(a=a, b=b, c=c, order=7),
+                Nsat=Nsat,
+                a=a,
+                b=b,
+                c=c,
+            )
+            # integrate the number counts over the bin, set order for computational speed.
+            I = romberg_integrator(func, (bin_edges[bin], bin_edges[bin + 1]), order=5)
+            return I 
+
+        # G-test requires that sum_i O_i = sum_i E_i:
+        # the total of the two data set counts must be equal.
+       
         # generate expected data from chi2 fit:
-        def model(bin, a, b, c):
-            func = lambda x: satellite_number(
-                x=x,
-                A=get_normalization_constant(a=a, b=b, c=c),
-                Nsat=Nsat,
-                a=a,
-                b=b,
-                c=c,
-            )
-            # integrate the number counts over the bin, set order for computational speed.
-            I = romberg_integrator(func, (bin_edges[bin], bin_edges[bin + 1]), order=5)
-            return I / bin_widths[bin]
-
-        chi2_data = [model(bin, *p_chi2) for bin in np.arange(nbins)]
+        chi2_data = np.array([model(bin, *p_chi2) for bin in np.arange(nbins)])
+        E_tot = np.sum(chi2_data)
+        O_tot = np.sum(data)
+        chi2_data *= O_tot/E_tot
+        
         G_score_chi2 = Gtest(data, chi2_data)
-
-        # generate expected data from poisson fit:
-        def model(bin, a, b, c):
-            func = lambda x: satellite_number(
-                x=x,
-                A=get_normalization_constant(a=a, b=b, c=c),
-                Nsat=Nsat,
-                a=a,
-                b=b,
-                c=c,
-            )
-            # integrate the number counts over the bin, set order for computational speed.
-            I = romberg_integrator(func, (bin_edges[bin], bin_edges[bin + 1]), order=5)
-            return I / bin_widths[bin]
-
-        chi2_data = [model(bin, *p_poisson) for bin in np.arange(nbins)]
-        G_score_poisson = Gtest(data, chi2_data)
-
-        # Append the G and Q scores for chi2 and poisson fits to their respective arrays
         G_scores_chi2.append(G_score_chi2)
-        Q_scores_chi2.append(0.0)
+        
+        # generate expected data from poisson fit:
+        poisson_data = np.array([model(bin, *p_poisson) for bin in np.arange(nbins)])
+        E_tot = np.sum(poisson_data)
+        O_tot = np.sum(data)
+        poisson_data *= O_tot/E_tot
+        
+        G_score_poisson = Gtest(data, poisson_data)
         G_scores_poisson.append(G_score_poisson)
-        Q_scores_poisson.append(0.0)
+        
+        # the degrees of freedom is given by 
+        # k = N - M
+        # where
+        #   N is the number of data points: amount of bins
+        #   M is the number of independent parameters: 3
+        k = nbins - 3
+        print("k:", k)
+        
+        # we compute the Qscores by inputting Gscores:
+        Q_score_chi2 = Qscore(k, G_score_chi2)
+        Q_scores_chi2.append(Q_score_chi2)
+        Q_score_poisson = Qscore(k, G_score_poisson)
+        Q_scores_poisson.append(Q_score_poisson)
 
     # Save G and Q scores for chi2 and poisson fits to tex files for later use in the PDF
     with open("Calculations/statistical_test_table_rows.tex", "w") as f:
